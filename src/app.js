0e1f5bba42f6335cc38ebee87ea8dc871f24a9d5
Kaarel Loide <kaarel.loide@cyber.ee>
2019-10-31 19:18:17 +0200

    remove package-lock.json, and add basic scene changing

--- src/app.js
+++ src/app.js
@@ -1,23 +1,17 @@
 import * as THREE from 'three';
 
-var camera, scene, renderer;
-var geometry, material, mesh;
+var camera, currentScene, renderer;
 
 init();
 animate();
 
 function init() {
+	document.addEventListener("keypress", changeScene);
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+	camera.position.z = 10;
 
-	scene = new THREE.Scene();
-
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
-
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
+	currentScene = createScene1();
 
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
@@ -28,10 +22,57 @@ function init() {
 function animate() {
 
 	requestAnimationFrame( animate );
-
+	var mesh = currentScene.getObjectByName("mesh");
 	mesh.rotation.x += 0.01;
 	mesh.rotation.y += 0.02;
 
-	renderer.render( scene, camera );
+	renderer.render( currentScene, camera );
 
+}
+
+// change scene with number keys 1,2,3
+function changeScene(e) {
+	switch (e.code) {
+		case "Digit1":
+			currentScene = createScene1();
+			break;
+		case "Digit2":
+			currentScene = createScene2();
+			break;
+		case "Digit3":
+			currentScene = createScene3();
+			break;
+	}
+	console.log(e.code);
+
+}
+
+function createScene1() {
+	var scene = new THREE.Scene();
+	var geometry = new THREE.BoxGeometry(6, 6, 6);
+	var material = new THREE.MeshNormalMaterial();
+	var mesh = new THREE.Mesh( geometry, material );
+	mesh.name = "mesh";
+	scene.add( mesh );
+	return scene; 
+}
+
+function createScene2() {
+	var scene = new THREE.Scene();
+	var geometry = new THREE.SphereGeometry(5, 32, 32);
+	var material = new THREE.MeshNormalMaterial();
+	var mesh = new THREE.Mesh( geometry, material );
+	mesh.name = "mesh";
+	scene.add( mesh );
+	return scene; 
+}
+
+function createScene3() {
+	var scene = new THREE.Scene();
+	var geometry = new THREE.TorusGeometry(3, 1, 16, 100);
+	var material = new THREE.MeshNormalMaterial();
+	var mesh = new THREE.Mesh( geometry, material );
+	mesh.name = "mesh";
+	scene.add( mesh );
+	return scene; 
 }
